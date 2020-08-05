3bbba9c960932effb1c813c1a7c392a1e07b0611
Dec <dec@Decs-MacBook-Air.local>
2020-08-05 14:58:51 +0100

    encapsulate tests in a class

--- test/test_addition.py
+++ test/test_addition.py
@@ -1,7 +1,8 @@
 from mathematic.utils import add
 
-def test_answer():
-    assert add(1) == 2
+class TestClass():
+    def test_answer(self):
+        assert add(1) == 2
 
-def test_wrong():
-    assert add(2) == 3
+    def test_wrong(self):
+        assert add(2) == 3

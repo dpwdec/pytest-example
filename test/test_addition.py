a5dd8ed47a5fa8529ee5cc906b0eac8f2557bac9
Dec <dec@Decs-MacBook-Air.local>
2020-08-05 15:51:19 +0100

    add conftest fixtures

--- test/test_addition.py
+++ test/test_addition.py
@@ -1,4 +1,9 @@
 from mathematic.utils import add
+import pytest
+
+@pytest.fixture
+def create_message():
+    return "Message"
 
 class TestClass():
     def test_answer(self):
@@ -6,3 +11,9 @@ class TestClass():
 
     def test_wrong(self):
         assert add(2) == 3
+
+    def test_msg(self, create_message):
+        assert create_message == "Message"
+
+    def test_db(self, create_db):
+        assert create_db == "db"

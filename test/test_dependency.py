c7726bd63fe024d600b4fb776be9ceec90f991de
Dec Kolakowski <declankolakowski@gmail.com>
2020-08-16 19:10:10 +0100

    dep classing

--- test/test_dependency.py
+++ test/test_dependency.py
@@ -6,3 +6,13 @@ def test_dep(mocker):
     b.get_flavor.return_value = "Chocolate"
     a = Ape(b)
     assert a.eat_banana() == "I eat a banana with flavor Chocolate"
+
+def test_called(mocker):
+    b = mocker.Mock()
+    # b.get_flavor.return_value = ""
+    a = Ape(b)
+    a.eat_banana()
+    b.get_flavor.assert_called_with()
+
+def test_called_dep(mocker):
+    pass
